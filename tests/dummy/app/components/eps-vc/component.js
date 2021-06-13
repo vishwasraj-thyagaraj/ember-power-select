97a73f83e583dd2b943b10ec36f00a6e56939b03
vishwasraj <vishwasraj.thyagaraj@freshworks.com>
2021-06-13 20:13:32 +0530

    Updates 200K

--- tests/dummy/app/components/eps-vc/component.js
+++ tests/dummy/app/components/eps-vc/component.js
@@ -7,33 +7,32 @@ import ParallelJS from '../../utils/parallel';
 
 let t0, t1;
 
-t0 = performance.now();
-window.users = [];
-for (let i = 0, j = USERNAMES.length; i < j; i += 10000) {
-  let p = new ParallelJS(USERNAMES.slice(i , i + 10000));
-  p.spawn(filterNames).then(function (data) {
-    users = [...users, ...data];
-    console.log(users);
-  });
-}
-t1 = performance.now();
-console.log(`Single Threaded loop took ${t1 - t0} milliseconds.`);
-
-t0 = performance.now();
 function filterNames(data) {
   let arr = [];
   for(let i=0; i<data.length; i++) {
     let option = data[i];
-    if(option['name'].toUpperCase().indexOf('vishwas'.toUpperCase()) >= 0) {
+    if(option['name'].toUpperCase().indexOf('Vishwas'.toUpperCase()) >= 0) {
       arr.push(option);
     }
   }
   return arr;
 }
 
-filterNames(USERNAMES);
+t0 = performance.now();
+let users = [];
+for (let i = 0, j = USERNAMES.length; i < j; i += 10000) {
+  let p = new ParallelJS(USERNAMES.slice(i , i + 10000));
+  p.spawn(filterNames).then(function (data) {
+    users = [...users, ...data];
+  });
+}
 t1 = performance.now();
 console.log(`Multi Threaded took ${t1 - t0} milliseconds.`);
+
+t0 = performance.now();
+filterNames(USERNAMES);
+t1 = performance.now();
+console.log(`Single Threaded loop took ${t1 - t0} milliseconds.`);
 
 export default Component.extend({
   selected: null,
